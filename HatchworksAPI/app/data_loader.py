345a9e509e5026160c1c6d510f5567926ed5e9a9
R S <rspet123@gmail.com>
2022-01-18 03:12:21 -0500

    Add files via upload
    Added tests, delete, and a while ton of other stuff

--- HatchworksAPI/app/data_loader.py
+++ HatchworksAPI/app/data_loader.py
@@ -1,20 +1,19 @@
+"""data loaders and other data interacting functions for interacting with the json file"""
 import json
+import os
+class DataLoader:
+    """Set of functions to help loading and unloading data"""
+    dirname = os.path.dirname(__file__)
+    recipe_path = (dirname+"\\recipes.json")
 
-class data_loader:
-    recipe_path = "app/resources/recipes.json"
-    
-    
-    def __init__(self,path: str):
-        recipe_path = path
     #our overloaded constructor, to make sure we can do other stuff, not just recipes
-    def __init__(self):
-        recipe_path = "app/resources/recipes.json"
-        
-    
+
+
     @classmethod
-    def load(self):
+    def load(cls):
+        """Loads JSON file to a dict"""
         recipe_dict = {}
-        with open(self.recipe_path,"r") as data_file:
+        with open(cls.recipe_path,"r") as data_file:
             recipe_list = json.load(data_file).get("recipes")
         #It gives us a list, which is okay
         #but we kind of want it to be faster, and we want to be able to find
@@ -24,34 +23,33 @@ class data_loader:
             recipe_dict[recipe["name"]] = {"ingredients":recipe["ingredients"],
                                            "instructions":recipe["instructions"]}
         return recipe_dict
-    
-    @classmethod 
-    def load_specific(self,search: str):
-        with open(self.recipe_path,"r") as data_file:
-            return json.load(data_file).get(search)
-    
 
-        
+    # Simply updates the recipe on the dict, but does not *save* it
+    # or write it to the disk
     @classmethod
-    def update_recipe(self, recipe: dict, updated_recipe: dict):
+    def update_recipe(cls, recipe: dict, updated_recipe: dict):
+        """Update Recipe"""
         for key, val in updated_recipe.items():
             recipe[key] = val
-    
+
+    # Helper method to allow us to turn the dict that we made when loading
+    # back into a list, to allow us to save it on the disk
+    # exactly how it was
     @staticmethod
     def recipe_dict_to_list(recipe_dict: dict):
+        """Helper Method, dict -> List"""
         recipe_list =[]
         for recipe_name in recipe_dict.keys():
-           recipe_list.append({"name":recipe_name,
+            recipe_list.append({"name":recipe_name,
              "ingredients":recipe_dict[recipe_name]["ingredients"],
              "instructions":recipe_dict[recipe_name]["instructions"]
              })
         return recipe_list
-        
+    # we use this to write to disk
     @classmethod
-    def write(self, recipe_dict: dict):
-        recipe_list = self.recipe_dict_to_list(recipe_dict)
-        with open(self.recipe_path, "w") as data_file:
+    def write(cls, recipe_dict: dict):
+        """Write Dict to disk"""
+        recipe_list = cls.recipe_dict_to_list(recipe_dict)
+        with open(cls.recipe_path, "w") as data_file:
             json.dump({"recipes": recipe_list}, data_file, indent=2)
-
-
-
+            
